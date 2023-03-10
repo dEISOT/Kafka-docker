b117371b021d29d1375192abd827f163332b80b7
CyJay96 <konstantin201586@gmail.com>
2023-03-10 17:16:32 +0300

    add producer&consumer for test messages

---
+++ src/Consumer/Program.cs
@@ -0,0 +1,33 @@
+﻿using Confluent.Kafka;
+using static Confluent.Kafka.ConfigPropertyNames;
+
+var tokenSource = new CancellationTokenSource();
+Console.CancelKeyPress += (sender, eventArgs) =>
+{
+    tokenSource.Cancel();
+};
+
+var consumerConfig = new ConsumerConfig
+{
+    BootstrapServers = "localhost:9092",
+    GroupId = args[0] ?? "test-consumer-group",
+    AutoOffsetReset = AutoOffsetReset.Latest
+};
+
+var consumer = new ConsumerBuilder<Null, string>(consumerConfig)
+    .Build();
+
+const string topic = "test-topic";
+consumer.Subscribe(topic);
+
+while (!tokenSource.IsCancellationRequested)
+{
+    var message = consumer.Consume(new TimeSpan(0,0,1));
+    if(message != null)
+    {  
+        Console.WriteLine(message.TopicPartitionOffset + " " + message.Value);
+    }
+
+}
+
+consumer.Close();
